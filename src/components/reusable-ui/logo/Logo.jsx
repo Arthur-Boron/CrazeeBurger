5a44b6fa16c789577a93507b14b3dfa3f2c8dfc7
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-03 22:28:05 +0200

    add Logo crazee burger

--- src/components/reusable-ui/logo/Logo.jsx
+++ src/components/reusable-ui/logo/Logo.jsx
@@ -4,13 +4,40 @@ import { styled } from 'styled-components'
 function Logo() {
   return (
     <LogoStyled>
-      Crazee Burger
+      <h1>
+        Crazee
+      </h1>
+      <img src='/images/logo-burger.png' alt='logo-crazee-burger'/>
+      <h1>
+        Burger
+      </h1>
     </LogoStyled>
   )
 }
 
 const LogoStyled = styled.div`
-  background: darkgreen;
+  color: white;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h1 {
+    display: inline;
+    text-align: center;
+    font-size: 36px;
+    line-height: 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+  }
 `
 
 export default Logo
