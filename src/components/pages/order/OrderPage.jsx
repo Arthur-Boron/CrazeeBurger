dacbb515a84feb8fab63ad4a05d88dcc9131b617
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-04 21:02:13 +0200

    Add container layout

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { styled } from 'styled-components'
+import { theme } from '../../../theme'
 
 function OrderPage() {
 
   const {name} = useParams()
 
   return (
-    <div>
-      <h1>Bonjour {name}</h1>
-      <br />
-      <Link to={'/'}>
-          <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className='container'>
+        <h1>Bonjour {name}</h1>
+        <br />
+        <Link to={'/'}>
+            <button>Déconnexion</button>
+        </Link>
+      </div>
+      
+    </OrderPageStyled>
     
   )
 }
+
+const OrderPageStyled = styled.div`
+  background-color: orange;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background-color: red;
+    height: 98vh;
+    width: 98vw;
+  }
+`
 
 export default OrderPage
