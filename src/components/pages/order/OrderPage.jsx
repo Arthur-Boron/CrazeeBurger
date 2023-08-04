fc02bd0035d5f23f0de7022cd3830b85fab74e5c
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-04 23:18:20 +0200

    import background color with design system

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router'
 import { styled } from 'styled-components'
 import Navbar from '../navbar/Navbar'
 import Main from './Main'
+import { theme } from '../../../theme'
 
 
 function OrderPage() {
@@ -21,7 +22,7 @@ function OrderPage() {
 }
 
 const OrderPageStyled = styled.div`
-  background-color: orange;
+  background-color: ${theme.colors.primary};
   height: 100vh;
   display: flex;
   justify-content: center;
