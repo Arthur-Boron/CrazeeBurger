f967bcc5788b90745e289cac59048d151e00d733
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-05 17:13:04 +0200

    Start creating layout

---
+++ src/components/pages/menu/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { styled } from 'styled-components'
+import { theme } from '../../../theme'
+
+function Menu() {
+  return (
+    <MenuStyled>
+        Menu
+    </MenuStyled>
+  )
+}
+
+const MenuStyled = styled.div`
+    background-color: green;
+`
+
+export default Menu
--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { styled } from 'styled-components'
 import { theme } from '../../../theme'
+import Menu from '../menu/Menu'
+import Basket from '../basket/Basket'
 
 function Main() {
   return (
     <MainStyled>
-        
+      <Basket />
+      <Menu />
     </MainStyled>
   )
 }
@@ -15,6 +18,8 @@ const MainStyled = styled.main`
     flex-grow: 1;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
+    display: grid;
+    grid-template-columns: 25% 75%;
 `
 
 export default Main
