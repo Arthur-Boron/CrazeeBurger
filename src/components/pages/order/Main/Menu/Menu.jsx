f792d290950167ab9715ea4dff49969db1f4dc18
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-06 10:32:02 +0200

    Add product info

--- src/components/pages/order/Main/Menu/Menu.jsx
+++ src/components/pages/order/Main/Menu/Menu.jsx
@@ -10,24 +10,50 @@ function Menu() {
   return (
     <MenuStyled>
       {menu.map(product => {
-        return <div className="product" key={product.id}>{product.title}</div>
+        return (
+        <div className="product" key={product.id}>
+          <div className='image-container'>
+            <img className='img' src={product.imageSource} alt={product.title}/>
+          </div>
+          <div className='info-text'>
+            <div className='title'>{product.title}</div>
+            <div className='description'>
+              <div className="price">{product.price}</div>
+              <button>Ajouter</button>
+            </div>
+          </div>
+          
+        </div>
+        )
       })}
     </MenuStyled>
   )
 }
 
 const MenuStyled = styled.div`
-    background-color: green;
     padding: 50px 50px 150px;
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
     grid-row-gap: 60px;
     justify-items: center;
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     
     .product {
       background-color: darkviolet;
       width: 240px;
       height: 330px;
+      box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+      border-radius: ${theme.borderRadius.extraRound};
+
+      .image-container {
+        width: 100px;
+        height: auto;
+        
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
     }
 `
 
