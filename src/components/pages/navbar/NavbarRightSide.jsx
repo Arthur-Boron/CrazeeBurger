49a9992f71c1f00df9fbf8227f90b530fb40ec0f
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-06 16:14:06 +0200

    Add toggle Button

--- src/components/pages/navbar/NavbarRightSide.jsx
+++ src/components/pages/navbar/NavbarRightSide.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { styled } from 'styled-components'
 import { theme } from "../../../theme"
 import Profile from './Profile'
+import ToggleButton from '../../reusable-ui/ToggleButton'
 
 function NavbarRightSide({name}) {
   return (
     <NavbarRightSideStyled>
-        <div className='admin-button'>
-
-        </div>
+        <ToggleButton labelIfChecked={"DÉSACTIVER LE MODE ADMIN"} labelIfUnchecked={"ACTIVER LE MODE ADMIN"}/>
         <Profile name={name}/>
     </NavbarRightSideStyled>
   )
