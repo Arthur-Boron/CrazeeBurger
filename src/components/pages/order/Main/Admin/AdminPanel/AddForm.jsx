23803031abc225d5fba373945770c52c5e7f046f
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-09 17:14:47 +0200

    Add content addForm and editForm in adminPanel

---
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+function AddForm() {
+  return (
+    <AddFormStyled>
+        AddForm
+    </AddFormStyled>
+  )
+}
+
+const AddFormStyled = styled.form`
+
+`
+
+export default AddForm
