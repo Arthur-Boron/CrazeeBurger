96f0d9df92435228f01e40964725dff217a51bc5
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-08-22 14:31:23 +0200

    Use addForm component to create EditForm - when click, display product info

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,18 +1,68 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import HintMessage from './HintMessage'
 import OrderContext from '../../../../../../context/OrderContext'
+import { styled } from 'styled-components'
+import { theme } from '../../../../../../theme'
+import TextInput from '../../../../../reusable-ui/TextInput'
+import ImagePreview from './ImagePreview'
+import getInputConfig from '../../../../../../config/addFormInputConfig'
 
 function EditForm() {
 
-  const {productSelected} = useContext(OrderContext)
+  const {productSelected, setProductSelected} = useContext(OrderContext)
+
+  const handleChange = (event) => {
+    const newValue = event.target.value
+    const propertyName = event.target.name
+    setProductSelected({...productSelected, [propertyName]: newValue})
+  }
+
+  const inputs = getInputConfig(productSelected)
 
   return (
-    <div>
-      <HintMessage />
-      <span>{productSelected && productSelected.title}</span>
-    </div>
+    // <div>
+    //   <HintMessage />
+    //   <span>{productSelected && productSelected.title}</span>
+    // </div>
+    <EditFormStyled action='submit'>
+        <ImagePreview imageSource={productSelected.imageSource} title={productSelected.title} />
+        <div className='inputFields'>
+            {
+                inputs.map((input) => {
+                    return  <TextInput
+                        {...input}
+                        onChange={handleChange}
+                        version="dark"
+                        required 
+                    />
+                })
+            }
+        
+        </div>
+    </EditFormStyled>
       
   )
 }
+
+const EditFormStyled = styled.form`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    height: 100%;
+    grid-row-gap: ${theme.spacing.xs};
+    grid-column-gap: ${theme.spacing.md};
+    
+    .inputFields {
+        grid-area: 1/2/4/2;
+        display: grid;
+        grid-template-rows: repeat(3, 1fr);
+        margin-right: 20px;
+    } 
+
+    .submitButton {
+        grid-area: 4/2/5/3;
+        width: 50%;
+    } 
+`
 
 export default EditForm
