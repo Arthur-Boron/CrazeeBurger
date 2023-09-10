0e65b9681a14bbd89c8310e2658f26ebe158d42f
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-09-11 00:17:16 +0200

    animate adminPanel when open

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,20 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { styled } from 'styled-components'
-import { theme } from '../../../../../theme'
 import AdminTabs from './AdminTabs'
 import AdminPanel from './AdminPanel/AdminPanel'
 import AdminTabContext from '../../../../../context/AdminTabContext'
+import { motion, useAnimation } from 'framer-motion';
 
 function Admin() {
 
   const {isCollapsed} = useContext(AdminTabContext)
+  const panelAnimationControls = useAnimation();
+
+  const panelVariants = {
+    open: { opacity: 1, height: 'auto', transition: { duration: 0.3 } },
+    closed: { opacity: 0, height: 0, transition: { duration: 0.3 } },
+  };
+
+  useEffect(() => {
+    if (isCollapsed) {
+      panelAnimationControls.start('closed');
+    } else {
+      panelAnimationControls.start('open');
+    }
+  }, [isCollapsed, panelAnimationControls]);
 
   return (
     <AdminStyled>
-        <AdminTabs />
-        {!isCollapsed && <AdminPanel />}
+      <AdminTabs />
+        <motion.div
+          initial="closed"
+          animate={panelAnimationControls}
+          variants={panelVariants}
+        >
+          {!isCollapsed && <AdminPanel />}
+        </motion.div>
     </AdminStyled>
-  )
+  );
 }
 
 const AdminStyled = styled.div`
