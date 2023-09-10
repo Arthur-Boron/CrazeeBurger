0dcf8d1fd6bd156e65f67e63375f21af8e1ebbaa
Arthur-Boron <130967232+Arthur-Boron@users.noreply.github.com>
2023-09-11 00:20:00 +0200

    clean adminPanel animation

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { styled } from 'styled-components'
 import AdminTabs from './AdminTabs'
 import AdminPanel from './AdminPanel/AdminPanel'
@@ -8,27 +8,18 @@ import { motion, useAnimation } from 'framer-motion';
 function Admin() {
 
   const {isCollapsed} = useContext(AdminTabContext)
-  const panelAnimationControls = useAnimation();
 
   const panelVariants = {
     open: { opacity: 1, height: 'auto', transition: { duration: 0.3 } },
     closed: { opacity: 0, height: 0, transition: { duration: 0.3 } },
   };
 
-  useEffect(() => {
-    if (isCollapsed) {
-      panelAnimationControls.start('closed');
-    } else {
-      panelAnimationControls.start('open');
-    }
-  }, [isCollapsed, panelAnimationControls]);
-
   return (
     <AdminStyled>
       <AdminTabs />
         <motion.div
           initial="closed"
-          animate={panelAnimationControls}
+          animate={isCollapsed ? 'closed' : 'open'}
           variants={panelVariants}
         >
           {!isCollapsed && <AdminPanel />}
